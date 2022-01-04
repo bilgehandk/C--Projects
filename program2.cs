03438369785633adcdc214d65ee9b41a26f13378
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2022-01-04 11:02:06 +0300

    program2- variables

---
+++ program2.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace ProgrammingAssignment2
+{
+    // IMPORTANT: Only add code in the section
+    // indicated below. The code I've provided
+    // makes your solution work with the 
+    // automated grader on Coursera
+
+    /// <summary>
+    /// Programming Assignment 1
+    /// </summary>
+    class Program2
+    {
+        
+
+        /// <summary>
+        /// Programming Assignment 1
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+            byte b = 5; //1 byte
+            sbyte c = 5; //1 byte
+
+            short s = 5;  //2 byte
+            ushort us = 5; //2 byte
+
+            Int16 i16 = 2; //2 byte
+            int i = 2; //4 byte
+            Int32 i32 = 2; //4 byte
+            Int64 i64 = 2; //8 byte
+
+            uint ui = 2; //4 byte
+            long l = 4; //8 byte
+            ulong ul = 4; //8 byte
+
+            float f = 5; //4 byte
+            double d = 5; //8 byte
+            decimal de = 5; //16 byte
+
+            char ch = '2'; // 2 byte
+            string str = "Bilgehan"; //sınırsız
+
+
+            DateTime dt = DateTime.Now; //
+            Console.WriteLine(dt);
+
+
+
+
+
+
+        }
+    }
+}
+        
