7957928799d31fa8b29f4c55649d68cedf4805f6
Bilgehan Demirkaya <b.demirkaya@ug.bilkent.edu.tr>
2022-01-12 14:42:24 +0300

    metotlar

---
+++ metotlar.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace C__Projects
+{
+    class metotlar
+    {
+        static void Main(string[] args)
+        {
+            //erişim_belirteci geri_donustipi metot_adi(parameterlistesi/arguman)
+            //{
+                //komutlar;
+            //}
+            int a = 2;
+            int b = 3;
+
+
+            Console.WriteLine(a+b);
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArtirveTopla(ref a,ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a+b)); 
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return (deger1 + deger2);
+        }
+    }
+
+    public class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArtirveTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+            return deger1 + deger2;
+        }
+    }
+}
